f015e354807126daaecb1022f945063d883cd534
Ajno <39632696+AjnoEO@users.noreply.github.com>
2025-09-23 16:41:46 +0300

    Обновил перенаправление, теперь по идентификатору

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ var date = new Date();
 var milliseconds = date.getMilliseconds();
 poll_number = milliseconds % 10 < prob_first ? 1 : 2;
 
-const form_id = "68c546de90fa7b60e2338c49"; 
-const choice_id = 9008957735634384
-const params = {
-    1: 9008957735634394,
-    2: 1758480912996
-};
-window.location.replace("https://forms.yandex.ru/cloud/" + form_id + "?answer_choices_" + choice_id + "=" + params[poll_number]);
+const params = new URLSearchParams(window.location.search)
+const id = params.get("id")
+if (id === null) {
+    history.back()
+    const p = document.getElementById("text")
+    p.textContent = "Невалидная ссылка! Переходите по ссылке, полученной по почте"
+} else {
+    const formname = "test-forma"
+    const field_id = "68d265c8b32c53aae3fc50b7"
+    const answer_id = params.get("id") + poll_number
+    const url = `https://n7sz0w9a.forms.app/${formname}#${field_id}=${answer_id}`
+    window.location.replace(url);
+}
